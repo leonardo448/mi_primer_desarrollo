49953e7e72398410cf086e39c7921cb66a06481d
leonardo448 <mezaguerraleonardo@gmail.com>
2024-10-24 12:15:35 -0500

    mi-primer-commit

---
+++ area.js
@@ -0,0 +1,32 @@
+//area del cuadrado
+//let area, lado1;
+
+//lado1 = 9;
+
+
+//area = lado1 * lado1;
+
+//console.log("El area es: " + area);
+
+
+//area del rectangulo
+
+//let resultado, lad1, lad2;
+
+//lad1 = 10;
+//lad2 = 15;
+
+//resultado = lad1 * lad2;
+
+//console.log("El area es: " + resultado)
+
+
+//area del triamgulo
+//let igual, base, altura;
+
+//base = 5;
+//altura = 10;
+
+//igual = base * altura / 2;
+
+//console.log("El area es: " + igual)
